69f76ef346d3b29708d13ace7475ab12ebb2c535
Lucas <86076720+lucas-berth@users.noreply.github.com>
2022-04-05 20:01:30 -0500

    working on problems
    not completed, need some help figuring out the separate function and graphing

--- script 1 (outline).R
+++ script 1 (outline).R
@@ -9,7 +9,28 @@ chick_fil_a_data <- read_csv("/Users/lucasberth/Documents/Coding Docs/DSAA---Pro
 
 head(chick_fil_a_data)
 
+chick_fil_a_data %>%
+  separate('Serving size', into = "Serving Size(g)", sep = 'g')
+
+#still figuring out how to get the g off the number in the serving size column
 top5size <- chick_fil_a_data %>%
-  select(`Serving size`, Menu) %>%
-  slice(2)
+  separate('Serving size', into = "Serving Size(g)", sep = 'g') %>%
+  data_frame("Serving Size(g)" = numeric()) %>%
+  select("Menu", "Serving Size(g)") %>%
+  arrange(desc("Serving Size(g)")) %>%
+  slice(5)
   
+
+
+#question 2
+chick_fil_a_data %>%
+  select(Menu, Calories, `Sugar (G)`) %>%
+  arrange(desc(Calories)) %>%
+  slice(1:5)
+
+#trying a regression // problem with this one 
+chick_fil_a_data %>%
+  filter()
+  ggplot(aes("Protein (G)", "Calories")) +
+  geom_point()
+
